99bed1e94ea6d079eeb28112599c0e8059c72c8e
KING.JOSEPH <jkingara@student.bridgew.edu>
2022-03-30 16:10:53 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+
+input("which file would you like to open?")
+if input()= ("fortune.txt")
+    open("fortune.txt")
+
+#fortune file
+#created my own fortune file.
+
+def load_fortune():
+    file = open("fortune.txt", 'r')
+    sentences = file.readlines()
+    sayings = sentences.split("%")
+
+#loops
+#if statement
+#display function
+
+def display_fortune():
+    for i in sayings:
+        print(input("do you want another saying?"), "for sure or nope im good"
+        if input() == for sure:
+            print(f"new saying")
+        else
+        return()
+
+
+main()
